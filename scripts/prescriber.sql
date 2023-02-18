c63606995ca8e371d7324c05e380c22e6cfda416
Daytrieon Dean <Daytrieon@Daytrieons-Air.attlocal.net>
2023-02-18 12:45:39 -0600

    6

--- scripts/prescriber.sql
+++ scripts/prescriber.sql
@@ -89,11 +89,11 @@ FROM drug
 
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
-SELECT drug_name,
-CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
-	 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
-     ELSE 'neither' END AS drug_type
-FROM drug;
+-- SELECT drug_name,
+-- CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+-- 	 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+--      ELSE 'neither' END AS drug_type
+-- FROM drug;
 
 
 
@@ -186,20 +186,27 @@ UNION
 FROM prescriber);
 
 
-
-
 -- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
 
-SELECT prescriber.npi, specialty_description
-FROM prescriber
-LEFT JOIN prescription
-ON prescriber.npi = prescription.npi
-LEFT JOIN
-ON
+
+ 
 
 
 --     a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Managment') in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
 
+SELECT prescriber.npi, 
+	   specialty_description
+FROM prescriber
+LEFT JOIN drug
+ON prescriber.nppes_provider_first_name = drug.drug_name
+WHERE specialty_description = 'Pain Managment' 
+AND nppes_provider_city = 'NASHVILLE'
+AND drug.opioid_drug_flag = 'Y';
+
+
 --     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
     
+	
+	
+	
 --     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
